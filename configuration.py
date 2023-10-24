cd6eadbb876b48ba5b188fb64c0ac2d7e9421f3f
W4V3M <ya.jackass11@gmail.com>
2023-10-24 13:16:11 +0100

    Diplom

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://8fedc8cf-e367-4719-93f7-e5beb7997f1e.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Саша",
+    "lastName": "Смирнов",
+    "address": "Дмитровская 1",
+    "metroStation": 158,
+    "phone": "+79999999999",
+    "rentTime": 5,
+    "deliveryDate": "2023-10-24",
+    "comment": "бла бла бла",
+    "color": [
+        "BLACK"
+    ]
+}
